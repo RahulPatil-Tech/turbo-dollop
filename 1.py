22206226570a2aac83ea6c81d0ec08a0809c4ad8
Rahul Patil <81096780+RahulPatil-Tech@users.noreply.github.com>
2025-06-03 09:40:13 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+'''def add_to_li(value, my_list=[]): 
+    my_list.append(value) 
+    return my_list 
+print(add_to_li(1)) 
+print(add_to_li(2)) 
+print(add_to_li(3))'''
+'''
+print(r"python developer / python")'''
+
+'''print("python.upper")'''
+'''
+n1= float('7.8.9'.split('.')[2]) 
+n2= float('1.3.5'.partition('.')[2])
+diff = n1 - n2
+print(diff)
+print(n1)
+print(n2)
+'''
+'''for i in range(1,3):
+    for j in range(2):
+        print(i + j)'''
+'''s = "Python"
+print(s[6:])'''
+
+'''import array as arr 
+c = arr.array('f', [1.1, 2.2, 3.3]) 
+print(c[1])'''
